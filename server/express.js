50fa6b7b0e0c89f9154762f174ce74515c6bcce9
Sayah Reda <reda1998.rs@gmail.com>
2022-03-22 20:38:22 +0100

    set graphql queryroot

--- server/express.js
+++ server/express.js
@@ -1,5 +1,18 @@
-import express from 'express';
-const App = express();
-App.listen(4000, () => {
-  console.log('now listining for request on port 4000');
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/shema');
+
+const app = express();
+
+// bind express with graphql
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: true
+  })
+);
+
+app.listen(4000, () => {
+  console.log('now listening for requests on port 4000');
 });
